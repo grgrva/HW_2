7535837289e910a4b487716b084eff8d62146b11
elena.an.grigoryeva <elena.an.grigoryeva@lenta.com>
2026-01-05 15:26:35 +0400

    delete conftest.py update test_web.py

--- test_web.py
+++ test_web.py
@@ -1,3 +1,21 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def driver():
+    opts = Options()
+
+    opts.add_argument("--headless=new")
+    opts.add_argument("--window-size=1280,900")
+    driver = webdriver.Chrome(options=opts)
+    yield driver
+
+    driver.quit()
+
+
 url_google = 'https://www.google.com/'
 url_github = 'https://github.com/'
 
